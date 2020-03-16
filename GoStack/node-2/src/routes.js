8a0abb11df514b39253bf6976bdcc6059f8c9171
juanoude <juanoude@hotmail.com>
2020-03-16 06:14:13 -0300

    Conexão e primeira inserção teste

--- GoStack/node-2/src/routes.js
+++ GoStack/node-2/src/routes.js
@@ -1,7 +1,19 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.get('/', async (req, res) => {
+  
+  let user = await User.create({
+    name: "Juan Ananda",
+    email: "juanoude@hotmail.com",
+    password_hash: "123teste123"
+  });
+ 
+  return res.json(user);
+});
+
 
 export default routes;
