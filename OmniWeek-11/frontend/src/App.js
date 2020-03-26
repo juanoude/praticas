697bb0236ca0e36983d63070aac903b382747e51
juanoude <juanoude@hotmail.com>
2020-03-26 20:29:20 -0300

    Basic structure and login page design

---
+++ OmniWeek-11/frontend/src/App.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import Logon from './pages/Logon';
+
+import './global.css'
+
+function App() {
+  
+  return (
+    <div>
+      <Logon />
+    </div>
+  );
+}
+
+export default App;
---
+++ OmniWeek-11/frontend/src/pages/Logon/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import './styles.css';
+import logoImg from '../../assets/logo.svg';
+import heroesImg from '../../assets/heroes.png';
+import {FiLogIn} from 'react-icons/fi';
+
+function Logon() {
+  return (
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoImg} alt="Be the Hero"/>
+      
+        <form>
+          <h1>Faça seu logon</h1>
+
+          <input placeholder="Sua ID"/>
+          <button type="submit" className="button">Entrar</button>
+
+          <a href="register">
+            <FiLogIn size={16} color="#E02041" />
+             Não tenho cadastro
+          </a>
+        </form>
+      </section>
+
+      <img src={heroesImg} alt="Heroes"/>
+    </div>
+  );
+}
+
+export default Logon;
