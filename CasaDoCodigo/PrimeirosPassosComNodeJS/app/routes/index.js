63f996e47dc97404baaf10e2d702e6b459dab6d3
juanoude <juanoude@hotmail.com>
2020-04-02 23:43:29 -0300

    Nunjucks and express

--- CasaDoCodigo/PrimeirosPassosComNodeJS/app/routes/index.js
+++ CasaDoCodigo/PrimeirosPassosComNodeJS/app/routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/tabuada/:valor', (req, res) => {
+  var valor = req.params.valor;
+  var resultado = (valor % 2) == 0 ? valor : false;
+  res.render('tabuada', {numero: resultado});
+});
+
 module.exports = router;
