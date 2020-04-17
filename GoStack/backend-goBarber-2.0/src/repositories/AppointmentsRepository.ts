c4b86e97006ce076460750ece9a21c4eb99f5d6f
juanoude <juanoude@hotmail.com>
2020-04-17 13:03:58 -0300

    repositorie logic for creating and finding

---
+++ GoStack/backend-goBarber-2.0/src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,28 @@
+import { isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
+
+class AppointmentsRepository {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  public create(provider: string, date: Date): Appointment {
+    const appointment = new Appointment(provider, date);
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    const existsDate = this.appointments.find(appointment =>
+      isEqual(appointment.date, date)
+    );
+
+    return existsDate || null;
+  }
+}
+
+export default AppointmentsRepository;
--- GoStack/backend-goBarber-2.0/src/routes/appointments.routes.ts
+++ GoStack/backend-goBarber-2.0/src/routes/appointments.routes.ts
@@ -1,26 +1,23 @@
 import { Router } from 'express';
-// import { uuid } from 'uuidv4';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
-import Appointment from '../models/Appointment';
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
 
-const appointments: Appointment[] = [];
+const appointments = new AppointmentsRepository();
 
 appointmentsRouter.post('/', (req, res) => {
   const { provider, date } = req.body;
 
   const parsedDate = startOfHour(parseISO(date));
 
-  if (appointments.find(appointment => isEqual(appointment.date, parsedDate))) {
+  if (appointments.findByDate(parsedDate)) {
     return res
       .status(400)
       .json({ message: 'This appointment is not avaiable' });
   }
 
-  const appointment = new Appointment(provider, parsedDate);
-
-  appointments.push(appointment);
+  const appointment = appointments.create(provider, date);
 
   return res.json(appointment);
 });
