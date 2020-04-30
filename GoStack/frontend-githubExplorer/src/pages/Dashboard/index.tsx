b05407feba8262c0572ff607262d217722b82931
juanoude <juanoude@hotmail.com>
2020-04-29 21:01:10 -0300

    conecting to github API logic

--- GoStack/frontend-githubExplorer/src/pages/Dashboard/index.tsx
+++ GoStack/frontend-githubExplorer/src/pages/Dashboard/index.tsx
@@ -1,48 +1,58 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
+import api from '../../services/api';
 
 import {Title, Form, Repositories} from './styles';
 import logoImg from '../../assets/logo.svg';
-import profilePhoto from '../../assets/photo3.jpeg';
 import { FiChevronRight } from 'react-icons/fi';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+    const response = await api.get<Repository>(`/repos/${newRepo}`);
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer"/>
       <Title>Explore Repositórios no Github</Title>
 
-      <Form action="#">
-        <input type="text" placeholder="Digite o nome do repositório"/>
+      <Form onSubmit={handleAddRepository}>
+        <input type="text"
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"/>
+
         <button type="submit"> Pesquisar </button>
       </Form>
 
       <Repositories>
-        <a href="#">
-          <img src={profilePhoto} alt="profile image"/>
-          <div>
-            <strong>juanoude/ribon-challenge</strong>
-            <p> Nice challenge from a nice job interview</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
 
-        <a href="#">
-          <img src={profilePhoto} alt="profile image"/>
-          <div>
-            <strong>juanoude/ribon-challenge</strong>
-            <p> Nice challenge from a nice job interview</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map( repository => (
+          <a href="#">
+            <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+            <div>
+              <strong> {repository.full_name} </strong>
+              <p> {repository.description} </p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
 
-        <a href="#">
-          <img src={profilePhoto} alt="profile image"/>
-          <div>
-            <strong>juanoude/ribon-challenge</strong>
-            <p> Nice challenge from a nice job interview</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
       </Repositories>
 
 
