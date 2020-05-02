698ce7004da09483344aac3cc53f7756e59f425b
juanoude <juanoude@hotmail.com>
2020-05-02 08:59:52 -0300

    githubexplorer finished and working perfectly

--- GoStack/frontend-githubExplorer/src/pages/Repository/index.tsx
+++ GoStack/frontend-githubExplorer/src/pages/Repository/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
+import api from '../../services/api';
 
-import { FiChevronLeft } from 'react-icons/fi';
-import { Header, RepositoryInfo } from './styles';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Header, RepositoryInfo, Issues } from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import repoImg from '../../assets/photo3.jpeg';
@@ -11,48 +12,91 @@ interface RouteParams {
   repository: string;
 }
 
+interface Repository {
+  stargazers_count: number;
+  open_issues_count: number;
+  forks_count: number;
+  description: string;
+  owner: {
+    avatar_url: string;
+    login: string;
+  }
+  full_name: string;
+}
+
+interface Issue {
+  id: number;
+  html_url: string;
+  title: string;
+  user: {
+    login: string;
+  }
+}
+
+
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RouteParams>();
+
+  const [issues, setIssues] = useState<Issue[]>([]);
+  const [repository, setRepository] = useState<Repository | null>(null);
+
+  useEffect(() => {
+    api.get<Repository>(`/repos/${params.repository}`).then((response) =>  {
+      setRepository(response.data);
+    }, null);
+
+    api.get<Issue[]>(`/repos/${params.repository}/issues`).then((response) => {
+      setIssues(response.data);
+    });
+  }, [params.repository]);
+
   return (
     <>
       <Header>
         <img src={logoImg} alt="Github Explorer"/>
-
         <Link to='/'>
           <FiChevronLeft size={20} />
           <span> Voltar a página </span>
         </Link>
-
       </Header>
+      {repository && (
+        <RepositoryInfo>
+          <header>
+            <img src={repository.owner.avatar_url} alt="Repository photo"/>
+              <div>
+                <strong> {repository.full_name} </strong>
+                <p> {repository.description} </p>
+              </div>
+          </header>
 
-      <RepositoryInfo>
-        <header>
-          <img src={repoImg} alt="Repository photo"/>
+          <ul>
+            <li>
+              <strong>{repository.stargazers_count}</strong>
+              <span>Stars</span>
+            </li>
+            <li>
+              <strong> {repository.forks_count} </strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong> {repository.open_issues_count} </strong>
+              <span>Issues</span>
+            </li>
+          </ul>
+        </RepositoryInfo>
+      )}
+
+      <Issues>
+        {issues.map((issue) => (
+          <a key={issue.id} href={issue.html_url}>
             <div>
-              <strong> Repository </strong>
-              <p> {params.repository} </p>
+              <strong> {issue.title} </strong>
+              <p> {issue.user.login} </p>
             </div>
-        </header>
-
-        <ul>
-          <li>
-            <strong>1808</strong>
-            <span>Stars</span>
-          </li>
-          <li>
-            <strong>48</strong>
-            <span>Forks</span>
-          </li>
-          <li>
-            <strong>67</strong>
-            <span>Issues</span>
-          </li>
-        </ul>
-      </RepositoryInfo>
-
-
-
-
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </Issues>
     </>
   );
 }
--- GoStack/frontend-githubExplorer/src/pages/Repository/styles.ts
+++ GoStack/frontend-githubExplorer/src/pages/Repository/styles.ts
@@ -55,7 +55,6 @@ export const RepositoryInfo = styled.section`
     list-style: none;
 
     li {
-
       text-decoration: none;
       & + li {
         margin-left: 80px;
@@ -73,5 +72,50 @@ export const RepositoryInfo = styled.section`
       }
     }
   }
+`;
 
+
+export const Issues = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    background: #FFF;
+    border-radius: 5px;
+    text-decoration: none;
+    padding: 24px;
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    div {
+      margin: 0 16px;
+      flex: 1;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #3d3d4d;
+    }
+  }
 `;
